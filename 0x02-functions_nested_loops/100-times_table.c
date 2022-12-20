7fa69aa6f33cef3151ed8a6ab5dab7b8f6a92267
tommyiversonj <tommyiversonj@gmail.com>
2022-12-19 19:20:06 -0800

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_times_table - Print table
+ * @n: Table for printing
+ *
+ * return: Nothing
+ */
+
+void print_times_table(int n)
+{
+  int row, col, r;
+
+  if (n >= 0 && n <= 15)
+    {
+      for (row = 0; row <= n; row++)
+	{
+	  col = 0;
+	  for (col = 0; col <= n; col++)
+	    {
+	      r = row * col;
+	      if (col != n)
+		{
+		  if (row + r < 10)
+		    {
+		      printf("%d,   ", r);
+		    }
+		  else if (row + r >= 10 && row + r < 100)
+		    {
+		      printf("%d,  ", r);
+		    }
+		  else
+		    {
+		      printf("%d, ", r);
+		    }
+		}
+	      else
+		{
+		  printf("%d", r);
+		}
+	    }
+	  printf("\n");
+	}
+    }
+}
---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+  long int sum;
+  int start;
+
+  start = 0;
+  sum = 0;
+  while (start < 1024)
+    {
+      if (start % 3 == 0  || start % 5 == 0)
+	{
+	  sum = sum + start;
+	}
+      start++;
+    }
+  printf("%li\n", sum);
+  return (0);
+}
