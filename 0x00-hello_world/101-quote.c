19309a611ed9cbd76713bc8ed4120de3598d9a5d
Tommy Iverson Johnson <75326197+tommyiversonj@users.noreply.github.com>
2023-07-14 01:57:23 +0000

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,7 +12,8 @@
 */
 int main(void)
 {
-    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(STDERR_FILENO, message, strlen(message));
-    return (1);
+
+const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(STDERR_FILENO, message, strlen(message));
+return (1);
 }
