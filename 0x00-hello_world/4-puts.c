3ce60414248b8daeae0efdd767277a866d570402
Tommy Iverson Johnson <75326197+tommyiversonj@users.noreply.github.com>
2023-07-14 01:05:10 +0000

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+*main - Main function of my project
+*
+*
+*Return: return 0 everytime
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
