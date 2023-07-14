329a4b6cf577f19647deb0a19cf0eb3bcfbd1c9c
Tommy Iverson Johnson <75326197+tommyiversonj@users.noreply.github.com>
2023-07-14 02:09:40 +0000

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,19 +1,13 @@
+#include <stdio.h>
 #include <unistd.h>
-#include <string.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-
 /**
-*main - main function
+* main - main function in the task
 *
 *
-*Return: return 0
+*Return: return 1
 */
 int main(void)
 {
-  
-const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(STDERR_FILENO, message, strlen(message));
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
