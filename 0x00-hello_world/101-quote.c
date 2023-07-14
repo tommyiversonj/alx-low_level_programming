3f400ac3191ce7611f4cd653d920849a5d48ef0c
tommyiversonj <tommyiversonj@gmail.com>
2023-07-13 18:44:50 -0700

    Last updates

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+	int main(){
+            const char* message = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
+	    write(STDERR_FILENO, message, strlen(message));
+	    return 1;
+	}
