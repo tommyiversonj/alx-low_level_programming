e34a877a80b9b307a57201a545734e3b0c302deb
Tommy Iverson Johnson <75326197+tommyiversonj@users.noreply.github.com>
2023-07-14 01:13:02 +0000

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
 *main - Main function
 *
@@ -8,8 +7,6 @@
 */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");	    
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-
 }
-	
