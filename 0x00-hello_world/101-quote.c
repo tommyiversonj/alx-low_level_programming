ccd92735814176582ab38d550696b4a4b956d284
Tommy Iverson Johnson <75326197+tommyiversonj@users.noreply.github.com>
2023-07-14 01:50:20 +0000

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,12 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+/**
+*main - main function
+*
+*
+*Return: return 0
+*/
 int main() {
     const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
     write(STDERR_FILENO, message, strlen(message));
