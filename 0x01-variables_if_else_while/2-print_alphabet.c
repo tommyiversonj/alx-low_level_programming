65bb3ae3c72e8a84eb59558dbf547066ea3d2ed2
Tommy Iverson Johnson <75326197+tommyiversonj@users.noreply.github.com>
2023-07-17 21:37:09 +0000

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,11 +8,12 @@
 int main(void)
 {
 	int letter;
+	
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		letter = tolower(letter);
 		putchar(letter);
 	}
-	putchar('\n');	
-return (0);
+	putchar('\n');
+	return (0);
 }
