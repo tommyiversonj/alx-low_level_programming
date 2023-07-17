e0bf4be48bc25a05e97d9a762dbaded4440bd101
Tommy Iverson Johnson <75326197+tommyiversonj@users.noreply.github.com>
2023-07-17 21:59:39 +0000

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,22 +1,18 @@
 #include <stdio.h>
 
 /**
- *  * main- Entry point
- *   * @void: Null value
- *    *
- *     * Description: Print numbers from 0 to 9
- *      * Return: Zero value
- *       */
-
+ * main - prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line, using putchar
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
 
-	for (n = '0'; n <= '9'; n++)
+	for (n = 48; n < 58; n++)
+	{
 		putchar(n);
-
+	}
 	putchar('\n');
-
 	return (0);
-
 }
