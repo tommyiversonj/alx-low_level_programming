9196c7cd94b1d6ec0d613deb179f3dc9760b386a
Tommy Iverson Johnson <75326197+tommyiversonj@users.noreply.github.com>
2023-07-18 23:17:20 +0000

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,18 @@
-#include "main.h"
+#include"main.h"
 
 /**
- * _isalpha - check for alphabetic character.
+ * _isalpha - checks if character is a letter
+ *            both lowercase or uppercase
  *
- * @c: parameter
- * Return: Always 0.
- */
+ * @c: takes input from other functions.
+ *
+ * Return: 1 is c if true else 0
+*/
+
 int _isalpha(int c)
 {
-  if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-    {
-      return (1);
-    }
-  return (0);
+	if (c >= 97 && c <= 122 &&
+	    c >= 65 && c >= 90)
+		return (1);
+	return (0);
 }
