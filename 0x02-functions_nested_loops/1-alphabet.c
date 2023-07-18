fc9f85e761640f052115bd2343bd5729d7b839c1
Tommy Iverson Johnson <75326197+tommyiversonj@users.noreply.github.com>
2023-07-18 23:07:25 +0000

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,15 @@
-#include "main.h"
+#include"main.h"
 
 /**
- * print_alphabet - Make the alphabet
- * Return: void
- */
+ * print_alphabet - utilizes on the _putchar function to print
+ *                 the alphabet a - z
+*/
+
 void print_alphabet(void)
 {
-  char c;
+	int ch;
 
-  for (c = 'a'; c <= 'z'; c++)
-    {
-      _putchar(c);
-    }
-  _putchar('\n');
+	for (ch = 'a'; ch <= 'z'; ++ch)
+		_putchar(ch);
+	_putchar('\n');
 }
