3b6438eebd41ac999114bf58595e6a06fe2ce683
Tommy Iverson Johnson <75326197+tommyiversonj@users.noreply.github.com>
2023-07-22 01:07:13 +0000

    Update 2-main.c

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -1,23 +1,22 @@
-#include <stdio.h>
-#include "main.h"
+#include "../main.h"
 
 /**
  * main - prints the largest of 3 integers
  * Return: 0
- */
+*/
 
 int main(void)
 {
-  int a, b, c;
-  int largest;
+	int a, b, c;
+	int largest;
 
-  a = 8;
-  b = 1;
-  c = 5;
+	a = 972;
+	b = -98;
+	c = 0;
 
-  largest = largest_number(a, b, c);
+	largest = largest_number(a, b, c);
 
-  printf("%d is the largest number\n", largest);
+	printf("%d is the largest number\n", largest);
 
-  return (0);
+	return (0);
 }
