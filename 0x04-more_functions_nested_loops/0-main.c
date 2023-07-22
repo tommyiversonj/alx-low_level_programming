90198295940d82ff199f9e0265ce846a11007161
Tommy Iverson Johnson <75326197+tommyiversonj@users.noreply.github.com>
2023-07-22 01:46:57 +0000

    Update 0-main.c

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -1,18 +1,18 @@
-#include "main.h"
-#include <stdio.h>
+#include "../main.h"
 
 /**
- * main - checks the code
+ * main - check the code for ALX School students.
  *
- * Return: Always 0.
- */
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
 {
-  char c;
+	char c;
 
-  c = 'A';
-  printf("%c: %d\n", c, _isupper(c));
-  c = 'a';
-  printf("%c: %d\n", c, _isupper(c));
-  return (0);
+	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isupper(c));
+	return (0);
 }
