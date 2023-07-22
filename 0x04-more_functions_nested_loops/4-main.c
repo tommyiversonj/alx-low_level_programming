dd052a283a660f8633a1d396cabe85935d87f579
Tommy Iverson Johnson <75326197+tommyiversonj@users.noreply.github.com>
2023-07-22 01:53:46 +0000

    Update 4-main.c

--- 0x04-more_functions_nested_loops/4-main.c
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -1,12 +1,13 @@
-#include "main.h"
+#include "../main.h"
 
 /**
- * main - check the code
+ * main - check the code for ALX School students
  *
- * Return: Always 0.
- */
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
 {
-  print_most_numbers();
-  return (0);
+	print_most_numbers();
+	return (0);
 }
