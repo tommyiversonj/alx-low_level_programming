0e7079f2b8e02a93693ce33e87a6476bf4e514a2
Tommy Iverson Johnson <75326197+tommyiversonj@users.noreply.github.com>
2023-07-22 02:07:04 +0000

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,21 +1,28 @@
 #include "main.h"
 
 /**
- * more_numbers - prints 10 times the numbers, from 0 to 14
- *followed by a new line
- */
+ * more_numbers - print 0 - 14 ten times and you
+ *		can only use _putchar three times
+ *
+ * Return: Always 0 (Success)
+*/
+
 void more_numbers(void)
 {
-  int i, j;
+	int num, row, count;
 
-  for (i = 0; i < 10; i++)
-    {
-      for (j = 0; j < 15; j++)
+	for (row = 1; row <= 10; ++row)
 	{
-	  if (j >= 10)
-	    _putchar(j / 10 + '0');
-	  _putchar(j % 10 + '0');
+		for (count = 0; count <= 14; ++count)
+		{
+			num = count;
+			if (count > 9)
+			{
+				_putchar(1 + 48);
+				num = count % 10;
+			}
+			_putchar(num + 48);
+		}
+		_putchar('\n');
 	}
-      _putchar('\n');
-    }
 }
