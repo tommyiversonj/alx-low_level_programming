21798e4143cb2cc11c980c9389accdfca67050d1
Tommy Iverson Johnson <75326197+tommyiversonj@users.noreply.github.com>
2023-07-25 01:06:18 +0000

    Update 3-main.c

--- 0x05-pointers_arrays_strings/3-main.c
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - check the code for ALX School students.
  *
  * Return: Always 0.
- */
+*/
+
 int main(void)
 {
-  char *str;
+	char *str;
 
-  str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-  _puts(str);
-  return (0);
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	_puts(str);
+	return (0);
 }
