81ed53a360ca88e3f7eb565914edd2fd8e1bb97f
Tommy Iverson Johnson <75326197+tommyiversonj@users.noreply.github.com>
2023-07-25 01:12:17 +0000

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,16 @@
 #include "main.h"
-
 /**
- * swap_int - swaps the values of two integers
- *
- * @a: first integer.
- * @b: second integer.
- */
+* swap_int - swaps the value of int a and int b
+* Owned by Bwave/Bright
+* @a: first int
+* @b: second int
+* Return 0
+*/
 void swap_int(int *a, int *b)
 {
-  int temp = *a;
+		int s;
 
-  *a = *b;
-  *b = temp;
+		s = *a;
+		*a = *b;
+		*b = s;
 }
