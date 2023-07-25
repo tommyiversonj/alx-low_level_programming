a9cbc9704d864305e42f0bcd730b999441582c14
Tommy Iverson Johnson <75326197+tommyiversonj@users.noreply.github.com>
2023-07-25 01:05:47 +0000

    Update 2-main.c

--- 0x05-pointers_arrays_strings/2-main.c
+++ 0x05-pointers_arrays_strings/2-main.c
@@ -2,17 +2,18 @@
 #include <stdio.h>
 
 /**
- * main - check the code
+ * main - check the code for ALX School students.
  *
  * Return: Always 0.
- */
+*/
+
 int main(void)
 {
-  char *str;
-  int len;
+	char *str;
+	int len;
 
-  str = "My first strlen!";
-  len = _strlen(str);
-  printf("%d\n", len);
-  return (0);
+	str = "My first strlen!";
+	len = _strlen(str);
+	printf("%d\n", len);
+	return (0);
 }
