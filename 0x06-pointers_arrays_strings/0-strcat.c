5c2f455a1ec2d396c70481df68ee9a3a67bfada0
Tommy Iverson Johnson <75326197+tommyiversonj@users.noreply.github.com>
2023-07-28 23:59:55 +0000

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
 #include <string.h>
-/**
- *_strcat - concatenates two strings
- *@dest: A pointer to a character that will be changed
- *@src: A pointer to a character that will also be changed
- *Return: dest
- */
 
-char* concatenate_by_section(char *dest, char *src, int src_start, int src_end) {
+/**
+ * concatenate_by_section - Concatenate a section of a string to another string.
+ * @dest: The destination string.
+ * @src: The source string.
+ * @src_start: The starting index of the section in the source string.
+ * @src_end: The ending index (exclusive) of the section in the source string.
+ *
+ * Return: A pointer to the resulting destination string.
+ */
+char *concatenate_by_section(char *dest, char *src, int src_start, int src_end)
+{
     // Find the end of the destination string
     char *ptr = dest;
-    while (*ptr != '\0') {
+    while (*ptr != '\0')
+    {
         ptr++;
     }
 
     // Append the section of the source string to the destination string
     int i;
-    for (i = src_start; i < src_end && src[i] != '\0'; i++) {
+    for (i = src_start; i < src_end && src[i] != '\0'; i++)
+    {
         *ptr++ = src[i];
     }
 
