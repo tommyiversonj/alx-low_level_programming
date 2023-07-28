6243a99febb60b5759af8a058fe3ce7c326818d3
Tommy Iverson Johnson <75326197+tommyiversonj@users.noreply.github.com>
2023-07-28 23:57:21 +0000

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,20 +7,21 @@
  *Return: dest
  */
 
-int main() {
-    char str1[50] = "Hello, ";
-    char str2[] = "world!";
-    
-    printf("Before concatenation:\n");
-    printf("str1: %s\n", str1);
-    printf("str2: %s\n", str2);
-    
-    // Concatenate a section of str2 to str1
-    concatenate_by_section(str1, str2, 0, 5); // Concatenate the first 5 characters of str2 to str1
-    
-    printf("\nAfter concatenation:\n");
-    printf("str1: %s\n", str1);
-    printf("str2: %s\n", str2);
-    
-    return 0;
+char* concatenate_by_section(char *dest, char *src, int src_start, int src_end) {
+    // Find the end of the destination string
+    char *ptr = dest;
+    while (*ptr != '\0') {
+        ptr++;
+    }
+
+    // Append the section of the source string to the destination string
+    int i;
+    for (i = src_start; i < src_end && src[i] != '\0'; i++) {
+        *ptr++ = src[i];
+    }
+
+    // Add a terminating null byte
+    *ptr = '\0';
+
+    return dest;
 }
