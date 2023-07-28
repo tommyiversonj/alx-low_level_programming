d7ff3764eb32cd10fcd1064dea97f9b73aba3d82
Tommy Iverson Johnson <75326197+tommyiversonj@users.noreply.github.com>
2023-07-28 23:54:52 +0000

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,5 @@
-#include "main.h"
+#include <stdio.h>
+#include <string.h>
 /**
  *_strcat - concatenates two strings
  *@dest: A pointer to a character that will be changed
@@ -6,24 +7,20 @@
  *Return: dest
  */
 
-char *_strcat(char *dest, char *src)
-{
-int i, j;
-
-i = 0;
-while (dest[i] != '\0')
-{
-i++;
-}
-
-j = 0;
-while (src[j] != '\0')
-{
-dest[i] = src[j];
-j++;
-i++;
-}
-dest[i] = '\0';
-
-return (dest);
+int main() {
+    char str1[50] = "Hello, ";
+    char str2[] = "world!";
+    
+    printf("Before concatenation:\n");
+    printf("str1: %s\n", str1);
+    printf("str2: %s\n", str2);
+    
+    // Concatenate a section of str2 to str1
+    concatenate_by_section(str1, str2, 0, 5); // Concatenate the first 5 characters of str2 to str1
+    
+    printf("\nAfter concatenation:\n");
+    printf("str1: %s\n", str1);
+    printf("str2: %s\n", str2);
+    
+    return 0;
 }
