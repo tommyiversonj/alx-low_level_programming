66d5ff49aad65e8054aadd12ce04a88b26b2f5fb
tommyiversonj <tommyiversonj@gmail.com>
2023-08-04 18:12:47 -0700

    Update

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include <stdio.h>
 
 /**
  * _putchar - writes the character c to stdout
@@ -7,7 +7,7 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
+int _putchar(char c) {
+    printf("%c", c);
+    return 0;
 }
---
+++ 0x09-static_libraries/include/_puts.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "_puts.h"
+
+void _puts(const char *str) {
+    // Function implementation
+    printf("%s\n", str);
+}
