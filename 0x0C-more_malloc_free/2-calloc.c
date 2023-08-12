a66e569ccc4fe8aa033cc813fecb0655d6ea3a5a
Tommy Iverson Johnson <75326197+tommyiversonj@users.noreply.github.com>
2023-08-12 01:02:26 +0000

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,32 +1,50 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * _calloc - Allocates memory for an array of a certain number
- *           of elements each of an inputted byte size.
- * @nmemb: The number of elements.
- * @size: The byte size of each array element.
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
  *
- * Return: If nmemb = 0, size = 0, or the function fails - NULL.
- *         Otherwise - a pointer to the allocated memory.
+ * Return: Nothing.
  */
-void *_calloc(unsigned int nmemb, unsigned int size)
+void simple_print_buffer(char *buffer, unsigned int size)
 {
-void *mem;
-char *filler;
-unsigned int index;
+    unsigned int i;
 
-if (nmemb == 0 || size == 0)
-return (NULL);
+    i = 0;
+    while (i < size)
+    {
+        if (i % 10)
+        {
+            printf(" ");
+        }
+        if (!(i % 10) && i)
+        {
+            printf("\n");
+        }
+        printf("0x%02x", buffer[i]);
+        i++;
+    }
+    printf("\n");
+}
 
-mem = malloc(size * nmemb);
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *a;
 
-if (mem == NULL)
-return (NULL);
-
-filler = mem;
-
-for (index = 0; index < (size * nmemb); index++)
-filler[index] = '\0';
-
-return (mem);
+    a = _calloc(98, sizeof(char));
+    strcpy(a, "Best");
+    strcpy(a + 4, " School! :)\n");
+    a[97] = '!';
+    simple_print_buffer(a, 98);
+    free(a);
+    return (0);
 }
