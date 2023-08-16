f80a3f95b31e07e6a4e374e506115bc235a83fd5
Tommy Iverson Johnson <75326197+tommyiversonj@users.noreply.github.com>
2023-08-16 23:44:29 +0000

    Update 0-main.c

--- 0x0F-function_pointers/0-main.c
+++ 0x0F-function_pointers/0-main.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include "function_pointers.h"
+
 /**
 * print_name_as_is - prints a name as is
  * @name: name of the person
- *
  * Return: Nothing.
  */
 void print_name_as_is(char *name)
 {
 printf("Hello, my name is %s\n", name);
 }
+
 /**
  * print_name_uppercase - print a name in uppercase
  * @name: name of the person
- *
  * Return: Nothing.
  */
 void print_name_uppercase(char *name)
@@ -35,9 +35,9 @@ putchar(name[i]);
 i++;
 }
 }
+
 /**
  * main - check the code
- *
  * Return: Always 0.
  */
 int main(void)
