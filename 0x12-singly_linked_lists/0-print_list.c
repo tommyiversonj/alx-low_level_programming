26fb2291cc08f2d19c13f4480e4b1b059c647641
Tommy Iverson Johnson <75326197+tommyiversonj@users.noreply.github.com>
2023-08-25 01:11:36 +0000

    Update 0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_list - Prints all the elements of a list
+ * @h: Pointer to the list to be printed
+ *
+ * Return: The number of node printed
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t node = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+        {
+			printf("[0] (nil)\n");
+        }
+        else
+        {
+			printf("[%u] %s\n", h->len, h->str);
+        }
+		h = h->next;
+		node++;
+	}
+	return (node);
+}
