fcde8d59c246b9e84ff18334aaa407970de43c80
Tommy Iverson Johnson <75326197+tommyiversonj@users.noreply.github.com>
2023-08-25 21:52:43 +0000

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,15 +14,15 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 	if (h->str == NULL)
-        {
+{
 	printf("[0] (nil)\n");
-        }
-        else
-        {
+}
+else
+{
 	printf("[%u] %s\n", h->len, h->str);
-        }
+}
 		h = h->next;
 		node++;
-	}
+}
 	return (node);
 }
